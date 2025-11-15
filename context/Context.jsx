cf450d1d6725c0b064198baafdcad9e93d8e4205
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2025-11-15 19:47:45 +0530

    ..

--- context/Context.jsx
+++ context/Context.jsx
@@ -4,18 +4,42 @@ import main from "../config/gemini";
 export const Context = createContext();
 
 const ContextProvider = ({ children }) => {
+  const delaypara=(index,nextword)=>{
+
+  }
   const [input, setInput] = useState("");
   const [recentPromt, setRecentPromt] = useState("")
   const [previousPromt, setPreviousPromt] = useState([]);
   const [showResult, setShowResult] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [resultData, setResultData] = useState("")
  
   const onSend= async(prompt) => {
-     main(input)
+  
+     setResultData("")
+     setRecentPromt(input)
+    setLoading(true)
+     setShowResult(true)
+    const response=await  main(input)
+    let responseArray=response.split("**");
+    let newArray;
+    for (let i=0;i<responseArray.length;i++) {
+      if (i===0||i%2!==1) {
+        newArray+=responseArray[i]
+      }
+      else{
+        newArray+="<b>"+responseArray[i]+"</b>"
+      }
+    }
+    let newArray2=newArray.split("*").join("</br>")
+    setResultData(newArray2)
+    setLoading(false)
+    setInput("")
+     
   }
 
   const contextValue = {
-      input,setInput,recentPromt,setRecentPromt,setPreviousPromt,setShowResult,setLoading,previousPromt,showResult,loading,onSend
+      input,setInput,recentPromt,setRecentPromt,setPreviousPromt,setShowResult,setLoading,previousPromt,showResult,loading,onSend,resultData
   };
 
   return <Context.Provider value={contextValue}>{children}</Context.Provider>;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,9 +12,21 @@ import main from "../../config/gemini.js";
 import { useContext } from "react";
 import { Context } from "../../context/Context.jsx";
 const Main = ({ visiable, setVisiable }) => {
- const {  input,setInput,recentPromt,setRecentPromt,setPreviousPromt,setShowResult,setLoading,previousPromt,showResult,loading,onSend}= useContext(Context)
- console.log(input);
- 
+  const {
+    input,
+    setInput,
+    recentPromt,
+    setRecentPromt,
+    setPreviousPromt,
+    setShowResult,
+    setLoading,
+    previousPromt,
+    showResult,
+    loading,
+    onSend,resultData
+  } = useContext(Context);
+  console.log(input);
+
   return (
     <div
       className={`p-5 w-full flex   flex-col ${visiable ? "ml-40" : "ml-20"}`}
@@ -24,68 +36,84 @@ const Main = ({ visiable, setVisiable }) => {
         <img src={user} alt="" className="w-10 rounded-4xl" />
       </nav>
       <main className="lg:mx-50 mt-20 ">
-        <section className="mb-20">
-          <h1 className="text-5xl  font-semibold mb-6">
-            <span className="text-transparent bg-clip-text bg-linear-to-r from-blue-400 via-purple-500 to-pink-500">
-              Hello, Dev.
-            </span>
-          </h1>
-          <h2 className="text-6xl font-semibold text-gray-300">
-            How can I help you today?
-          </h2>
-        </section>
-        <section className="grid grid-cols-2  lg:grid-cols-4  mt-10  ">
-          <div className="bg-gray-200 m-2 p-4 min-h-[200px] flex flex-col rounded-2xl justify-between hover:bg-gray-300 transition-all duration-200 cursor-pointer">
-            <p>Suggest beautiful places to see on an uncoming road trip</p>
-            <div>
-              <img
-                src={compass}
-                alt=""
-                className="w-6 justify-self-end   bg-white rounded-4xl "
-              />
-            </div>
-          </div>
+        {!showResult ? (
+          <>
+            <section className="mb-20">
+              <h1 className="text-5xl  font-semibold mb-6">
+                <span className="text-transparent bg-clip-text bg-linear-to-r from-blue-400 via-purple-500 to-pink-500">
+                  Hello, Dev.
+                </span>
+              </h1>
+              <h2 className="text-6xl font-semibold text-gray-300">
+                How can I help you today?
+              </h2>
+            </section>
+            <section className="grid grid-cols-2  lg:grid-cols-4  mt-10  ">
+              <div className="bg-gray-200 m-2 p-4 min-h-[200px] flex flex-col rounded-2xl justify-between hover:bg-gray-300 transition-all duration-200 cursor-pointer">
+                <p>Suggest beautiful places to see on an uncoming road trip</p>
+                <div>
+                  <img
+                    src={compass}
+                    alt=""
+                    className="w-6 justify-self-end   bg-white rounded-4xl "
+                  />
+                </div>
+              </div>
 
-          <div className="bg-gray-200 m-2 p-4 min-h-[200px] flex flex-col rounded-2xl justify-between hover:bg-gray-300 transition-all duration-200 cursor-pointer">
-            <p>Briefly summarize this concept: urban planning</p>
-            <div>
-              <img
-                src={bulb}
-                alt=""
-                className="w-6 justify-self-end   bg-white rounded-4xl "
-              />
-            </div>
-          </div>
+              <div className="bg-gray-200 m-2 p-4 min-h-[200px] flex flex-col rounded-2xl justify-between hover:bg-gray-300 transition-all duration-200 cursor-pointer">
+                <p>Briefly summarize this concept: urban planning</p>
+                <div>
+                  <img
+                    src={bulb}
+                    alt=""
+                    className="w-6 justify-self-end   bg-white rounded-4xl "
+                  />
+                </div>
+              </div>
 
-          <div className="bg-gray-200 m-2 p-4 min-h-[200px] flex flex-col rounded-2xl justify-between hover:bg-gray-300 transition-all duration-200 cursor-pointer">
-            <p>Brainstorm team bonding activities for our work retreat</p>
-            <div>
-              <img
-                src={message}
-                alt=""
-                className="w-6 justify-self-end   bg-white rounded-4xl "
-              />
+              <div className="bg-gray-200 m-2 p-4 min-h-[200px] flex flex-col rounded-2xl justify-between hover:bg-gray-300 transition-all duration-200 cursor-pointer">
+                <p>Brainstorm team bonding activities for our work retreat</p>
+                <div>
+                  <img
+                    src={message}
+                    alt=""
+                    className="w-6 justify-self-end   bg-white rounded-4xl "
+                  />
+                </div>
+              </div>
+              <div className="bg-gray-200 m-2 p-4 min-h-[200px] flex flex-col rounded-2xl justify-between hover:bg-gray-300 transition-all duration-200 cursor-pointer">
+                <p>Tell me about React js and React native</p>
+                <div>
+                  <img
+                    src={code}
+                    alt=""
+                    className="w-6 justify-self-end bg-white rounded-4xl  "
+                  />
+                </div>
+              </div>
+            </section>
+          </>
+        ) : (
+          <div className="max-h-[70vh] overflow-y-scroll p-5  hide-scrollbar">
+            <div className="flex gap-4 items-center mb-4">
+              <img src={user} alt=""  className="w-15 rounded-4xl "/>
+              <p className="text-2xl font-bold ">{recentPromt}</p>
             </div>
+            { loading&&
+              <div class="animate-spin inline-block size-6 border-4 border-current border-t-transparent text-blue-500 rounded-full" role="status" aria-label="loading">
+  <span class="sr-only">Loading...</span>
+</div>
+            }
+            <div dangerouslySetInnerHTML={{__html:resultData}}></div>
           </div>
-          <div className="bg-gray-200 m-2 p-4 min-h-[200px] flex flex-col rounded-2xl justify-between hover:bg-gray-300 transition-all duration-200 cursor-pointer">
-            <p>Tell me about React js and React native</p>
-            <div>
-              <img
-                src={code}
-                alt=""
-                className="w-6 justify-self-end bg-white rounded-4xl  "
-              />
-            </div>
-          </div>
-        </section>
-        <section className="w-full bg-gray-200 rounded-4xl p-5 flex justify-between gap-4 ">
+        )}
+        <section className={`w-full bg-gray-200 rounded-4xl p-5 flex justify-between gap-4 `}>
           <input
             value={input}
-            onChange={(e)=>setInput(e.target.value)}
+            onChange={(e) => setInput(e.target.value)}
             type="text"
             placeholder="Enter a prompt here"
             className="w-full outline-none"
-            
           />
           <div className="flex gap-1">
             <img
